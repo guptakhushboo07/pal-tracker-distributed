4d3daf3d0c63f5c584ee652cbdc124d3b5d60a54
guptakhushboo07 <guptakhushboo07@gmail.com>
2019-07-01 16:34:25 +0530

    updated file security

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -11,10 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestConfig {
 
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
+
 
     @Bean
     public ObjectMapper objectMapper() {
